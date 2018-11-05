7ae30ce9f3a01b70ec6b103df88af781feea359f
Chris Penner <christopher.penner@gmail.com>
2018-11-05 16:12:09 -0600

    Add zygo example

--- cheatsheet.md
+++ cheatsheet.md
@@ -2,11 +2,19 @@
 
 # Recursive
 
+## project 
+```{.haskell include=src/Examples/CheatSheet.hs snippet=project}
+```
+
 ## cata 
 ```{.haskell include=src/Examples/CheatSheet.hs snippet=cata}
 ```
 
 ## para 
 ```{.haskell include=src/Examples/CheatSheet.hs snippet=para}
+```
+
+## zygo 
+```{.haskell include=src/Examples/CheatSheet.hs snippet=zygo}
 ```
 
--- src/Examples/CheatSheet.hs
+++ src/Examples/CheatSheet.hs
@@ -5,9 +5,20 @@ import           Data.Functor.Foldable
 import           Data.TreeF
 import           Data.JSONF
 import qualified Data.Map                      as M
+import qualified Data.Set                      as S
 
--- start snippet cata
 -- |
+-- start snippet project
+-- project swaps the first layer of your type with the recursive functor
+-- version leaving the next layer untouched
+-- >>> project [1, 2, 3]
+-- Cons 1 [2,3]
+
+-- end snippet project
+
+
+-- |
+-- start snippet cata
 -- >>> sumList [1, 2, 3]
 -- 6
 
@@ -19,8 +30,8 @@ sumList = cata alg
   alg (Cons next total) = next + total
 -- end snippet cata
 
--- start snippet para
 -- |
+-- start snippet para
 -- >>> paraTails [1, 2, 3, 4]
 -- [[1,2,3,4],[2,3,4],[3,4],[4]]
 
@@ -31,3 +42,20 @@ paraTails = para alg
   alg Nil                   = []
   alg (Cons x (xs, tails')) = (x : xs) : tails'
 -- end snippet para
+
+-- |
+-- start snippet zygo
+-- >>> zygoDedupeList [1, 1, 3, 2, 3]
+-- [1,2,3]
+
+zygoDedupeList :: [Int] -> [Int]
+zygoDedupeList = zygo setBuilderAlg listBuilderAlg
+ where
+  setBuilderAlg :: ListF Int (S.Set Int) -> S.Set Int
+  setBuilderAlg Nil              = S.empty
+  setBuilderAlg (Cons n allNums) = S.insert n allNums
+  listBuilderAlg :: ListF Int (S.Set Int, [Int]) -> [Int]
+  listBuilderAlg Nil = []
+  listBuilderAlg (Cons n (allNums, ns)) =
+    if S.member n allNums then ns else n : ns
+-- end snippet zygo
