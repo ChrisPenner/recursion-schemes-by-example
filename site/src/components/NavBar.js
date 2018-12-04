dc34b9ba19b05756a631f088dd4e804153fdf3bb
Chris Penner <christopher.penner@gmail.com>
2018-12-03 18:20:05 -0600

    Update articles and navbar

--- site/src/components/NavBar.js
+++ site/src/components/NavBar.js
@@ -8,10 +8,16 @@ export default () =>
     <nav className="navbar" role="navigation" aria-label="main navigation">
         <div>
             <Link className="navbar-title" to="/">
-                    <img className="logo" width="24" height="24" src={icon} alt="logo" />
-                    <span className="title">{title}</span>
+                <img className="logo" width="24" height="24" src={icon} alt="logo" />
+                <span className="title">{title}</span>
             </Link>
+
             <span className="subtitle is-size-6"><i> A Chris Penner Project</i></span>
         </div>
-        <PatreonButton />
+        <Link className="nav-link" to="/">
+            Index
+        </Link>
+        <div className="patreon-button-nav">
+            <PatreonButton />
+        </div>
     </nav>
